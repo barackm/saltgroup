03f513870550c688c5cd9dc4c40a8279458c30fc
Baraka mukelenga <baraka.m@code-care.pro>
2025-01-18 18:30:08 +0200

    Remove unused metadata from EventDetailsPage for cleaner code

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -1,25 +1,5 @@
 import EventDetails from "@/components/events/EventDetails";
-import { Metadata } from "next";
 import React from "react";
-
-export const metadata: Metadata = {
-  title: "Event Details | SALT Events",
-  description:
-    "View detailed information about this premium event, including dates, venue, and booking options.",
-  openGraph: {
-    images: [
-      {
-        url: "/salt.png",
-        width: 1200,
-        height: 630,
-        alt: "SALT Events Banner",
-      },
-    ],
-  },
-  icons: {
-    icon: "/logo.png",
-  },
-};
 
 export default function EventDetailsPage() {
   return <EventDetails />;
